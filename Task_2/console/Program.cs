8fbcca9fbdee307fc2f81f76a5ffd1f0e7ffc164
Artem <artem.egorov.art2006@gmail.com>
2024-01-31 19:29:55 +0300

    решил задачу 2

---
+++ Task_2/console/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using lib;
+
+int count = 0;
+
+Console.WriteLine("Enter array length: ");
+
+count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+
+Console.WriteLine("Enter array elements: (press Enter after each element)");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Enter target: ");
+
+int target = 0;
+target = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(TaskTwo.FindIndex(array, target));       
---
+++ Task_2/lib/TaskTwo.cs
@@ -0,0 +1,33 @@
+﻿namespace lib;
+
+public class TaskTwo
+{
+    //Буду использовать бианрный поиск для решения задачи 2
+    static public int FindIndex(int[] array, int target)
+    {
+        if (array == null) {
+            return -1;
+        }
+
+        int left = 0;
+        int rigth = array.Length - 1;
+        int middle = 0;
+
+        while(left <= rigth) {
+            middle = left + (rigth - left) / 2;
+            if (array[middle] == target)
+            {
+                return middle;
+            } 
+            else if(array[middle] < target) {
+                left = middle + 1;
+            }
+            else {
+                rigth = middle - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
